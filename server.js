4812ac348b60e1a92120531ffb1f998589bd42dc
sideeg <satelite902@gmail.com>
2021-08-14 21:22:31 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,38 @@
-const app = require('express')()
-const http = require('http').createServer(app)
+const app = require('express')();
+const http = require('http').createServer(app);
 
 
 app.get('/', (req, res) => {
     res.send("Node Server is running. Yay!!")
-})
+});
 
 
 //Socket Logic
 const socketio = require('socket.io')(http)
 app.post('/sendMessage',(reg,res) =>{
-	socketio.on("connection", (userSocket) => {
+	let data = '';
+	
+
+	// res.write(JSON.parse(JSON.stringify(reg.body)));
+	reg.on('data', chunk => {
+		console.log(`Data chunk available: ${chunk}`)
+		data += chunk;
+	  })
+	  reg.on('end', () => {
+		console.log(`in the end data is : ${data}`);
+		// console.log(JSON.parse(JSON.stringify(data).user)); // 'Buy the milk'
+		console.log(JSON.parse(data));
+		socketio.on("connection", (userSocket) => {
 		userSocket.on("send_message", (data) => {
-			userSocket.broadcast.emit("receive_message", data)
+			userSocket.broadcast.emit("receive_message"+JSON.parse(data).user.id, data)
 		})
+		
 	})
-})
+    	res.end();
+	  })
+	res.write("done");
+	// res.end();
+});
 
 
-http.listen(process.env.PORT)
+http.listen(5000);
